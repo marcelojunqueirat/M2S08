f8f64f71abdb2920b3e5ea7f69cd34e73eda76d6
Marcelo Junqueira <marcelojxt@gmail.com>
2023-11-09 19:32:56 -0300

    [M02S08] Ex 01 - Mini-Projeto Checkin - Criação do projeto

---
+++ README.md
@@ -0,0 +1,3 @@
+### Exercicio modulo 02 semana 08
+
+#### Mini Projeto de registro de ponto
---
+++ src/test/java/com/m2s08/checkin/CheckinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2s08.checkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
